51f613347b669e653908f3fb04fe97c3f7dd97f2
MatheusNevs <matheusnf62@gmail.com>
2024-02-20 20:07:34 -0300

    fixing some things and private token

--- index.ts
+++ index.ts
@@ -4,6 +4,10 @@ const bodySchema = z.object({
   action: z.enum(["up", "down", "build"]),
   id: z.string(),
   address: z.string().optional(),
+  private: z.object({
+    token: z.string(),
+  }).optional(),
+  
 });
 
 const command = {
@@ -17,7 +21,10 @@ async function recordAction(data, paths) {
     // GRAVAR NO BD QUE COMECOU
     const proc = Bun.spawn(command[data.action], {
       cwd: paths[data.id],
-      env: data.address? { BRANCH: data.address} : {},
+      env: {
+        BRANCH: data.address? data.address : undefined,
+        TOKEN: data.private? data.private.token : undefined,
+      },
       stdin: "inherit",
       stdout: "inherit"
     })
@@ -26,47 +33,53 @@ async function recordAction(data, paths) {
     console.log("It worked")
     // GRAVAR NO BD QUE DEU CERTO
   } catch(e) {
-    console.log("It didn't work")
-    // GRAVAR NO BD QUE DEU ERRADO
+    if (e instanceof Error) {
+      console.log(`It didn't work - ${e.message}`)
+      // GRAVAR NO BD QUE DEU ERRADO
+    }
   }
 }
 
 const server = Bun.serve({
   port: Number(process.env.PORT) || 1357,
   async fetch(req) {
-    if (req.method !== "POST")
+    if (req.method !== "POST") {
+      console.log("Request is not valid") 
       return new Response("Invalid request", { status: 404 });
-    console.log("Request is valid")
+    }
     const body = await req.json();
 
     const authorization = req.headers.get("X-Authorization");
-    if (authorization !== process.env.API_KEY)
+    if (authorization !== process.env.API_KEY) {
+      console.log("Authorization key is not valid")
       return new Response("Deu errado 0!", {
         status: 401,
         statusText: "Unauthorized",
       });
-      console.log("Authorization key is valid")
+    }
     
     const result = bodySchema.safeParse(body);
     if (!result.success) {
+      console.log("Body is not valid")
       return new Response("Deu errado 1!", {
         status: 422,
         statusText: "Invalid body",
       });
     }
-    console.log("Body is valid")
 
     const paths = await Bun.file("./paths.json").json();
-    if (!(result.data.id in paths))
-      return new Response("Deu errado 5!", {
+    if (!(result.data.id in paths)) {
+      console.log("Path is not valid")
+      return new Response("Deu errado 2!", {
         status: 404,
         statusText: "Not Found",
       });
-    console.log("Path is valid")
-    
+    }
+      
     recordAction(result.data, paths);
-    return new Response(JSON.stringify({ message: "Deploy iniciado"}), {
+    return new Response("Deploy started", {
       status: 200,
+      statusText: "Sucess"
     })
 
   },
